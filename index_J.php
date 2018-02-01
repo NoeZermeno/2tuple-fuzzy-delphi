1809e5f1d148e5895a4eb4ea9deced48c08d8c43
Jeronimo Duran <32326203+Jeroduran@users.noreply.github.com>
2018-02-01 14:12:23 +0100

    new version of the pages Diseño, form and style01
    this is the new version of the page 2-tuple fuzzy

---
+++ index_J.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ('Diseño.php');?>
+	<div class="wrapp">
+			<div class="imagen">
+				<a><img src="A03.png" ></a>
+			</div>
+		</div>
+<div class="wrapp">  
+    <section class="main">
+		<div class="wrapp">
+			<div class="articulo">
+				<article>
+					<br><br>
+                    <p>  A 2tuple Linguistic Fuzzy Delphi method for validation of for validation of the construct of data collection questionnaires´.  </p><br>
+                    
+					<p>The 2tuples fuzzy-delphi website is a tool for validation of questionnaries previous to use as a tool for data collection. It  greatly help researches in the task of design and validate a data collection instrument such as an questionnaire. Prior its use, some properties must be guarantee, such as if it is valid for covering the construct or dimensions that it intends to evaluate. Thus, those researchers that want to know if that questionnaire is a good tool for data collection according with what it intends to measure, you should use this online tool.</p><br>
+                    
+					<p>The 2-Tuple Linguistic Fuzzy Delphi method is intended to be used with a panel of expert which assess a set of criteria regarding the validity of each item of the questionnaire. The process is iterative, accounts for consensus reaching between the experts, and the tools helps to visualize items with low rates or low consensus in order to guide the changes that enable a final solution: a consensual questionnaire to be apply in a research area.  </p><br>
+
+					<p> This   online solution is developed under PHP, HTML and CSS languages, thus is portable and easy to evolve and adapt to any particular situation. It is intended to be a tool to be use on every iteration of the method, by uploading a CSV file that incorportates the rates of every judge, over every criteria, over every item. Additionally, an expert weight can be incorporated to the computations. See the CSV format for more details regarding the input format sintax and significance.
+The main output of this open tool, is a visualization of the questionnaire scoring results item by item, and overall, together with other additional data (item weight, average, stardar deviation, etc) that enables to reach the degree of consensus needed By the use of the interface, some parameters of the model may help to perform trim operations over the questionnaire
+					</p><br><br>
+				</article>
+			</div>
+		</div>
+	<?php include ('footer.php') ?>
+    </section>
+</html>
