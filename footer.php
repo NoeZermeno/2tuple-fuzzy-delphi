23b54612d48a303d42abd8441928fabe6390666f
Jeovani Morales <escribeleaqui@gmail.com>
2018-02-03 15:12:41 +0100

    debugging files

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-		<div class="wrapp">
-			<p>License CC-By-NC-SA University of Granada
-            </p>
-            <p>Contact: rosana@ugr.es, jeovani@correo.ugr.es, nzermeno@correo.ugr.es, jeronimoduran@correo.ugr.es, herrera@decsai.ugr.es</p>
-		</div>
-</footer>
