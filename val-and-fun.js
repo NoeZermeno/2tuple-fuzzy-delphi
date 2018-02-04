997a341efc4ab197d41b825102d5ec7765e16fad
Jeronimo-Duran <jeronimoduran@correo.ugr.es>
2018-02-04 19:57:34 +0100

    js validate extension
    js for validate extensions when uploading

--- val-and-fun.js
+++ val-and-fun.js
@@ -7,43 +7,23 @@
  *
  * Date: 2016-05-20T17:17Z
  */
- 
-function show1(){
-      		document.getElementById("but01").style.display = "block";
-    }
-		function hide1(){
-      		document.getElementById("but01").style.display = "none";
-    }
 
-function fileValidation1(){
-    var fileInput = document.getElementsByName('file1');
-    var filePath = fileInput.value;
-    var allowedExtensions = /(.csv)$/i;
-    if(!allowedExtensions.exec(filePath)){
-        alert('Error! . . .  Please only upload files with .csv extension');
-        fileInput.value = '';
-        return false;
-	}
-} 
+ function show1(){
+       		document.getElementById("but01").style.display = "block";
+     }
 
-function fileValidation2(){
-    var fileInput = document.getElementByName('file2');
-    var filePath = fileInput.value;
-    var allowedExtensions = /(.csv)$/i;
-    if(!allowedExtensions.exec(filePath)){
-        alert('Error! . . .  Please only upload files with .csv extension');
-        fileInput.value = '';
-        return false;
-	}
-} 
+ function hide1(){
+       		document.getElementById("but01").style.display = "none";
+     }
 
-function fileValidation3(){
-    var fileInput = document.getElementByName('file3');
-    var filePath = fileInput.value;
-    var allowedExtensions = /(.csv)$/i;
-    if(!allowedExtensions.exec(filePath)){
-        alert('Error! . . .  Please only upload files with .csv extension');
-        fileInput.value = '';
-        return false;
-	}
-}
+ function fileValidation1(){
+     var fileInput = document.getElementsByName('file');
+     var filePath = fileInput.value;
+     var allowedExtensions = /(.csv)$/i;
+     if(!allowedExtensions.exec(filePath)){
+         alert('Error! . . .  Please only upload files with .csv extension');
+         fileInput.value = '';
+         document.getElementById("but01").style.display = "none";
+ 		return false;
+ 	}
+ }
