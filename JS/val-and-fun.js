d38eb54d0661136e46502853520eaec169698d6f
Jeronimo-Duran <jeronimoduran@correo.ugr.es>
2018-02-05 01:55:22 +0100

    organizing directories
    organizing directories

---
+++ JS/val-and-fun.js
@@ -0,0 +1,17 @@
+/*!
+ * jQuery JavaScript Library v1.12.4
+ * http://jquery.com/
+ * Copyright jQuery Foundation and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2016-05-20T17:17Z
+ */
+ 
+function show1(){
+      		document.getElementById("but01").style.display = "block";
+    }
+
+function hide1(){
+      		document.getElementById("but01").style.display = "none";
+    }
