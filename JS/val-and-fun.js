f7a62a7e96e75d6964bfe6e619ab139ba7eb7e10
Jeovani Morales <escribeleaqui@gmail.com>
2018-02-21 18:36:50 +0100

    Debug Files

--- JS/val-and-fun.js
+++ JS/val-and-fun.js
@@ -19,13 +19,13 @@ function ext_cont(f){
         var ext=['csv'];
         var v=f.value.split('.').pop().toLowerCase();
         for(var i=0,n;n=ext[i];i++){
-            if(n.toLowerCase()==v)
-                {show1();
-				return}
+            if(n.toLowerCase()==v){
+                show1();
+                return
+            }
               }
         var t=f.cloneNode(true);
         t.value='';
         f.parentNode.replaceChild(t,f);
         alert('The file extension is not valid. Please enter a file »name.csv«');
-		hide1();
 }
